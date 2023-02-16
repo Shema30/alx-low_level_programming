991bb6b81c57fd662fc421506ad9c81257aab79b
shema <shema@gmail.com>
2023-02-16 12:53:27 -0800

    5-printf.c

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -3,8 +3,6 @@
 * main - A program that print a line using the printf function
 * Return: 0 (success)
 */
-0x00. C - Hello, World
-BY LOTOLIM PHILIP PKIACH Email: philippkiach686@gmail.com Tel.0748036494 #DO HARD THINGS 
 int main(void)
 {
  printf("with proper grammar, but the outcome is a piece of art,\n");
